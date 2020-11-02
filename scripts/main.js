87e6a828780ceab0433481d3b1adcbe6e9053d9e
Ben OReillygit config --global user.email Ben <neb.oreilly@gmail.com>
2020-11-02 16:26:15 -0500

    nav updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,25 @@
-
-$(document).ready(function(){
-    $('.sidenav').sidenav({
-        inDuration: 600,
-        outDuration: 600,
-        edge: 'left',
-    });
-    $('.scrollspy').scrollSpy();
-    $(window).scroll(function(){ 
-        if ($(this).scrollTop() > 100) { 
-            $('#scroll').fadeIn(); 
-        } else { 
-            $('#scroll').fadeOut(); 
-        } 
-    }); 
-    $('#scroll').click(function(){ 
-        $("html, body").animate({ scrollTop: 0 }, 600); 
-        return false; 
-    });
-    $('#logo').click(function(){ 
-        $("html, body").animate({ scrollTop: 0 }, 600); 
-        return false; 
-    }); 
-    
-    
+$(document).ready(function () {
+  $(".sidenav").sidenav({
+    inDuration: 400,
+    outDuration: 400,
+    edge: "left",
+  });
+  $(".scrollspy").scrollSpy({
+    scrollOffset: 0,
+  });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $("#scroll").fadeIn();
+    } else {
+      $("#scroll").fadeOut();
+    }
+  });
+  $("#scroll").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
+  $("#logo").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
 });
-
