1886d0c8f8ed4553c4d2f96202c5d53c06d84580
Ben OReillygit config --global user.email Ben <neb.oreilly@gmail.com>
2021-02-02 14:03:48 -0500

    Hand Sanitizer page

--- scripts/main.js
+++ scripts/main.js
@@ -7,11 +7,24 @@ $(document).ready(function () {
   $(".scrollspy").scrollSpy({
     scrollOffset: 20,
   });
+  $(".slider").slider({
+    indicators: false,
+    height: 680,
+    transition: 500,
+  });
   $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
       $("#scroll").fadeIn();
     } else {
       $("#scroll").fadeOut();
+    }
+  });
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 100) {
+      $("nav").addClass("shadow");
+    } else {
+      $("nav").removeClass("shadow");
     }
   });
   $("#scroll").click(function () {
